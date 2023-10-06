782f581b79381e7bd97c38437bdbfe98faa2a048
melgazar9 <melgazar9@gmail.com>
2023-10-06 16:03:04 -0500

    saving progress

--- tap_yfinance/streams.py
+++ tap_yfinance/streams.py
@@ -2,65 +2,137 @@
 
 from __future__ import annotations
 
-import typing as t
 from pathlib import Path
+from singer_sdk.streams import Stream
+from typing import Iterable
+import logging
 
-from singer_sdk import typing as th  # JSON Schema typing helpers
+from datetime import datetime, timedelta
+import yfinance as yf
+import pandas as pd
+from skimpy import clean_columns
 
-from tap_yfinance.client import YFinanceStream
-
-# TODO: Delete this is if not using json files for schema definition
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
 
+# logger = logging.Logger()
 
-class UsersStream(YFinanceStream):
+def get_valid_yfinance_start_timestamp(interval, start='1950-01-01 00:00:00'):
+    """
+    Description
+    -----------
+    Get a valid yfinance date to lookback
+
+    Valid intervals with maximum lookback period
+    1m: 7 days
+    2m: 60 days
+    5m: 60 days
+    15m: 60 days
+    30m: 60 days
+    60m: 730 days
+    90m: 60 days
+    1h: 730 days
+    1d: 50+ years
+    5d: 50+ years
+    1wk: 50+ years
+    1mo: 50+ years --- Buggy!
+    3mo: 50+ years --- Buggy!
+
+    Note: Often times yfinance returns an error even when looking back maximum number of days - 1,
+        by default, return a date 2 days closer to the current date than the maximum specified in the yfinance docs
+    """
+
+    valid_intervals = ['1m', '2m', '5m', '15m', '30m', '60m', '1h', '90m', '1d', '5d', '1wk', '1mo', '3mo']
+    assert interval in valid_intervals, f'must pass a valid interval {valid_intervals}'
+
+    if interval == '1m':
+        start = max((datetime.today() - timedelta(days=5)), pd.to_datetime(start))
+    elif interval in ['2m', '5m', '15m', '30m', '90m']:
+        start = (max((datetime.today() - timedelta(days=58)), pd.to_datetime(start)))
+    elif interval in ['60m', '1h']:
+        start = max((datetime.today() - timedelta(days=728)), pd.to_datetime(start))
+    else:
+        start = pd.to_datetime(start)
+    start = start.strftime('%Y-%m-%d')  # yfinance doesn't like strftime with hours minutes or seconds
+
+    return start
+
+class YFinanceStream(Stream):
     """Define custom stream."""
 
-    name = "users"
-    path = "/users"
-    primary_keys: t.ClassVar[list[str]] = ["id"]
-    replication_key = None
-    # Optionally, you may also use `schema_filepath` in place of `schema`:
-    # schema_filepath = SCHEMAS_DIR / "users.json"  # noqa: ERA001
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property(
-            "id",
-            th.StringType,
-            description="The user's system ID",
-        ),
-        th.Property(
-            "age",
-            th.IntegerType,
-            description="The user's age in years",
-        ),
-        th.Property(
-            "email",
-            th.StringType,
-            description="The user's email address",
-        ),
-        th.Property("street", th.StringType),
-        th.Property("city", th.StringType),
-        th.Property(
-            "state",
-            th.StringType,
-            description="State name in ISO 3166-2 format",
-        ),
-        th.Property("zip", th.StringType),
-    ).to_dict()
+    name = "yfinance_stream"
+    schema = {
+        "type": "object",
+        "properties": {
+            "replication_key": {"type": "string"}
+        }
+    }
 
+    # the tap breaks when this is uncommented... need to figure out why
+    # def __init__(self):
+    #     self.failed_ticker_downloads = {}
+    #     self.column_order = ['replication_key', 'timestamp', 'timestamp_tz_aware', 'timezone', 'yahoo_ticker', 'open',
+    #     'high', 'low', 'close', 'volume', 'dividends', 'stock_splits']
 
-class GroupsStream(YFinanceStream):
-    """Define custom stream."""
+    def download_single_stock_price_history(self, ticker, yf_history_params=None):
+        """
+        Description
+        -----------
+        Download a single stock price ticker from the yfinance python library.
+        Minor transformations happen:
+            - Add column yahoo_ticker to show which ticker has been pulled
+            - Set start date to the minimum start date allowed by yfinance for that ticker (passed in yf_history_params)
+            - Clean column names
+            - Set tz_aware timestamp column to be a string
+        """
+        yf_history_params = self.yf_params.copy() if yf_history_params is None else yf_history_params.copy()
 
-    name = "groups"
-    path = "/groups"
-    primary_keys: t.ClassVar[list[str]] = ["id"]
-    replication_key = "modified"
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property("id", th.StringType),
-        th.Property("modified", th.DateTimeType),
-    ).to_dict()
+        assert 'interval' in yf_history_params.keys(), 'must pass interval parameter to yf_history_params'
+
+        if yf_history_params['interval'] not in self.failed_ticker_downloads.keys():
+            self.failed_ticker_downloads[yf_history_params['interval']] = []
+
+        if 'start' not in yf_history_params.keys():
+            yf_history_params['start'] = '1950-01-01 00:00:00'
+
+        yf_history_params['start'] = \
+            get_valid_yfinance_start_timestamp(interval=yf_history_params['interval'], start=yf_history_params['start'])
+
+        t = yf.Ticker(ticker)
+        try:
+            df = \
+                t.history(**yf_history_params) \
+                    .rename_axis(index='timestamp') \
+                    .pipe(lambda x: clean_columns(x))
+
+            self.n_requests += 1
+
+            if df is not None and not df.shape[0]:
+                self.failed_ticker_downloads[yf_history_params['interval']].append(ticker)
+                return pd.DataFrame(columns=self.column_order)
+        except:
+            self.failed_ticker_downloads[yf_history_params['interval']].append(ticker)
+            return
+
+        df.loc[:, self.yahoo_ticker_colname] = ticker
+        df.reset_index(inplace=True)
+        df['timestamp_tz_aware'] = df['timestamp'].copy()
+        df.loc[:, 'timezone'] = str(df['timestamp_tz_aware'].dt.tz)
+        df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True)
+        if self.convert_tz_aware_to_string:
+            df['timestamp_tz_aware'] = df['timestamp_tz_aware'].astype(str)
+
+        df.loc[:, "replication_key"] = df['timestamp'].astype(str) + '|' + df['yahoo_ticker'].astype(str)
+
+        df = df[self.column_order]
+        return df
+
+    def get_records(self, context: dict) -> Iterable[dict]:
+        # Define your list of tickers
+        tickers = ["AAPL", "MSFT", "GOOGL"]
+
+        yf_params = {'interval': '1m', 'start': '2023-10-01'}
+        for ticker_symbol in tickers:
+            ticker = yf.Ticker(ticker_symbol)
+            df = self.download_single_stock_price_history(ticker=ticker, yf_history_params=yf_params)
+            record = df.to_dict()
+            yield record
--- tap_yfinance/tap.py
+++ tap_yfinance/tap.py
@@ -17,29 +17,10 @@ class TapYFinance(Tap):
     # TODO: Update this section with the actual config values you expect:
     config_jsonschema = th.PropertiesList(
         th.Property(
-            "auth_token",
-            th.StringType,
-            required=True,
-            secret=True,  # Flag config as protected.
-            description="The token to authenticate against the API service",
-        ),
-        th.Property(
-            "project_ids",
-            th.ArrayType(th.StringType),
-            required=True,
-            description="Project IDs to replicate",
-        ),
-        th.Property(
             "start_date",
             th.DateTimeType,
             description="The earliest record date to sync",
-        ),
-        th.Property(
-            "api_url",
-            th.StringType,
-            default="https://api.mysample.com",
-            description="The url for the API service",
-        ),
+        )
     ).to_dict()
 
     def discover_streams(self) -> list[streams.YFinanceStream]:
@@ -49,8 +30,7 @@ class TapYFinance(Tap):
             A list of discovered streams.
         """
         return [
-            streams.GroupsStream(self),
-            streams.UsersStream(self),
+            streams.YFinanceStream(self),
         ]
 
 
